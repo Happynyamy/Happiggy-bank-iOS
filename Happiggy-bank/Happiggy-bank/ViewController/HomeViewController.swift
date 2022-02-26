5ac76041f15bc8ba4ec0294d148702c80ffe442b
sun <skkimeo@gmail.com>
2022-02-26 17:27:43 +0900

    [feat] 내비게이션 컨트롤러 추가 및 내비게이션 바 설정 #10

--- Happiggy-bank/Happiggy-bank/ViewController/HomeViewController.swift
+++ Happiggy-bank/Happiggy-bank/ViewController/HomeViewController.swift
@@ -50,6 +50,18 @@ class HomeViewController: UIViewController {
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     
     // MARK: - Button Actions(@objc functions)
     
