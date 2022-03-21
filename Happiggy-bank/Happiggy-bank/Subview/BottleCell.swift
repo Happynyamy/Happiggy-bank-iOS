3fea91844af9b1a10a5b0ddd767d1224a5f2a15e
Eunbin Kwon <kwoneunbini@gmail.com>
2022-03-21 23:49:31 +0900

    [feat] 저금통 리스트 에셋 변경 #36

--- Happiggy-bank/Happiggy-bank/Subview/BottleCell.swift
+++ Happiggy-bank/Happiggy-bank/Subview/BottleCell.swift
@@ -15,29 +15,17 @@ import Then
 final class BottleCell: UITableViewCell {
     
     // MARK: - Properties
-
-    /// 셀 배경이미지
-    @IBOutlet var bottleCellBackground: UIImageView!
-    
-    /// 저금통 프레임 이미지
-    @IBOutlet var bottleFrameImage: UIImageView!
-
-    /// 저금통 뚜껑 이미지
-    @IBOutlet var bottleCapImage: UIImageView!
     
     /// 저금통 제목 라벨
-    @IBOutlet var bottleTitleLabel: UILabel!
+    @IBOutlet weak var bottleTitleLabel: UILabel!
 
     /// 저금통 기간 라벨
-    @IBOutlet var bottleDateLabel: UILabel!
+    @IBOutlet weak var bottleDateLabel: UILabel!
 
     
     // MARK: - override func
     
     override func awakeFromNib() {
-        configureCellBackground()
-        configureCellBottleFrame()
-        configureCellBottleCap()
         configureLabels()
     }
     
@@ -56,27 +44,6 @@ final class BottleCell: UITableViewCell {
     
     
     // MARK: - cell settings
-    
-    // TODO: 인터페이스 빌더 상에 바로 추가?
-    // TODO: 에셋 이미지 이름 수정
-    /// 셀 배경 이미지 설정
-    private func configureCellBackground() {
-//        self.bottleCellBackground.image = UIImage(named: StringLiteral.backgroundImage)
-        self.bottleCellBackground.contentMode = .scaleAspectFill
-        self.bottleCellBackground.layer.cornerRadius = Metric.cornerRadius
-    }
-    
-    /// 셀 저금통 프레임 이미지 설정
-    private func configureCellBottleFrame() {
-        self.bottleFrameImage.image = UIImage(named: StringLiteral.bottleFrameImage)
-        self.bottleFrameImage.contentMode = .scaleAspectFill
-    }
-    
-    /// 셀 저금통 뚜껑 이미지 설정
-    private func configureCellBottleCap() {
-        self.bottleCapImage.image = UIImage(named: StringLiteral.bottleCapImage)
-        self.bottleCapImage.contentMode = .scaleAspectFill
-    }
     
     /// 셀 라벨 폰트사이즈, 색상 설정
     private func configureLabels() {
