805703bae7b1f4be0098ac6600184115431d612b
sun <skkimeo@gmail.com>
2022-04-02 22:51:25 +0900

    [feat] 저금통 개봉 시 쪽지 리스트로 넘어갈 때 리스트가 페이드인으로 나타나도록 변경 #94

--- Happiggy-bank/Happiggy-bank/ViewModel/BottleListViewModel.swift
+++ Happiggy-bank/Happiggy-bank/ViewModel/BottleListViewModel.swift
@@ -14,17 +14,20 @@ final class BottleListViewModel {
     /// 지난 저금통 리스트
     var bottleList: [Bottle]!
     
+
     /// 현재 열고 있는 저금통을 나타내는 프로퍼티로 있다면 해당 저금통 엔티티, 없으면 nil
-    var openingBottle: Bottle?
+    var openingBottle: Bottle? {
+        didSet {
+            guard let openingBottle = openingBottle
+            else { return }
+
+            self.updateOpeningBottleIndexPath(forBottle: openingBottle)
+        }
+    }
     
     /// 현재 열고 있는 저금통의 indexPath
-    var openingBottleIndexPath: IndexPath? {
-        guard let openingBottle = openingBottle,
-              let row = self.bottleList.firstIndex(of: openingBottle)
-        else { return nil }
-
-        return IndexPath(row: row, section: .zero)
-    }
+    var openingBottleIndexPath: IndexPath?
+    
     
     // MARK: - init
     init() {
@@ -55,5 +58,13 @@ final class BottleListViewModel {
 //        PersistenceStore.shared.deleteAll(Bottle.self)
         
         self.bottleList = list
+    }
+    
+    /// 개봉중인 저금통의 인덱스 패스 업데이트
+    private func updateOpeningBottleIndexPath(forBottle bottle: Bottle) {
+        guard let row = self.bottleList.firstIndex(of: bottle)
+        else { return }
+        
+        self.openingBottleIndexPath = IndexPath(row: row, section: .zero)
     }
 }
