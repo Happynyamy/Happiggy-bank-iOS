3c5c2acf7d9f925e49eb25c70cf97357293aaf30
sun <skkimeo@gmail.com>
2022-11-06 22:35:46 +0900

    [feat] 사진 기능을 위해 NoteDetailViewController 업데이트 #240

--- Happiggy-bank/Happiggy-bank/ViewModel/NoteDetailViewModel.swift
+++ Happiggy-bank/Happiggy-bank/ViewModel/NoteDetailViewModel.swift
@@ -11,68 +11,25 @@ import UIKit
 final class NoteDetailViewModel {
     
     // MARK: - Properties
-    
-    /// 쪽지들
-    var notes: [Note]!
-    
+
     /// 유저가 처음 선택한 쪽지의 인덱스
-    private(set) var selectedIndex: Int!
-    
+    let selectedIndex: Int
+
+    /// 쪽지 뷰 모델 배열
+    let noteViewModels: [PhotoNoteCellViewModel]
+
     /// 저금통 제목
-    private(set) var bottleTitle: String!
+    let bottleTitle: String!
     
     
     // MARK: - Init
     
     init(notes: [Note], selectedIndex: Int, bottleTitle: String) {
-        self.notes = notes
+        let count = notes.count
+        self.noteViewModels = notes.enumerated().map {
+            PhotoNoteCellViewModel(note: $0.element, index: $0.offset, numberOfTotalNotes: count)
+        }
         self.selectedIndex = selectedIndex
         self.bottleTitle = bottleTitle
-    }
-    
-    
-    // MARK: - Functions
-    
-    /// 연도를 색상을 바꿔서 문자열로 리턴
-    func attributedYearString(forNote note: Note) -> NSMutableAttributedString {
-        note.date
-            .yearString
-            .nsMutableAttributedStringify()
-            .color(color: .noteHighlight(for: note.color))
-    }
-    
-    /// 월, 일, 요일을 색상을 바꿔서 문자열로 리턴
-    func attributedMonthAndDayString(forNote note: Note) -> NSMutableAttributedString {
-        note.date
-            .monthDotDayWithDayOfWeekString
-            .nsMutableAttributedStringify()
-            .color(color: .noteHighlight(for: note.color))
-    }
-    
-    /// 색상에 따른 쪽지 이미지 리턴
-    func image(for note: Note) -> UIImage {
-        UIImage(named: StringLiteral.listNote + note.color.capitalizedString) ?? UIImage()
-    }
-    
-    /// 인덱스 라벨 문자열 리턴
-    func attributedIndexString(_ index: Int) -> NSMutableAttributedString {
-        let indexString = "\(index + 1)"
-            .nsMutableAttributedStringify()
-            .color(color: .customLabel)
-            .bold(fontSize: Font.indexLabel)
-        
-        let totalCountString = "/\(self.notes.count)"
-            .nsMutableAttributedStringify()
-        
-        indexString.append(totalCountString)
-        
-        return indexString
-    }
-    
-    /// 인덱스에 해당하는 쪽지의 색깔에 맞는 캐릭터 이미지 리턴
-    func characterImage(forIndex index: Int) -> UIImage {
-        let color = self.notes[index].color
-        
-        return .character(color: color)
     }
 }
