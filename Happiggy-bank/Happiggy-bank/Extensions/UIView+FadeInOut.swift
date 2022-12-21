5c4ab0b1942c30b7733fb4bca97a9cf081177aaf
sun <skkimeo@gmail.com>
2022-12-21 17:42:11 +0900

    [refactor] fade in/out isHidden을 사용하도록 변경 #253

--- Happiggy-bank/Happiggy-bank/Extensions/UIView+FadeInOut.swift
+++ Happiggy-bank/Happiggy-bank/Extensions/UIView+FadeInOut.swift
@@ -8,43 +8,77 @@
 import UIKit
 
 extension UIView {
-    
+
+    // MARK: - Properties
+
     /// 애니메이션 지속 시간: 0.2
     private static let animationDuration: TimeInterval = 0.2
-    
-    /// alpha 를 사용한 페이드 인 애니메이션: 디폴트 지속 시간 0.2, 딜레이 0, curveEaseInOut, completion nil
+
+
+    // MARK: - Functions
+
+    /// 해당 뷰를 애니메이션 효과와 함께 나타냄(IsHidden = false)
+    ///
+    /// 부모 뷰가 없는 경우 애니메이션 효과 없이 isHidden = false 로만 변경
+    /// - Parameters:
+    ///   - containerView: 해당 뷰의 부모 뷰, 입력하지 않으면 해당 뷰의 부모 뷰에 self.superview로 접근
+    ///   - duration: 애니메이션 지속 시간, 기본 값: 0.2
+    ///   - delay: 딜레이 시간, 기본 값: 0
+    ///   - options: 애니메이션 옵션, 기본 값: transitionCrossDissolve
+    ///   - completion: 애니메이션 종료 후 할 작업, 기본 값: nil
     func fadeIn(
-        withDuration duration: TimeInterval = animationDuration,
+        containerView: UIView? = nil,
+        duration: TimeInterval = animationDuration,
         delay: TimeInterval = .zero,
-        options: AnimationOptions = AnimationOptions.curveEaseInOut,
+        options: AnimationOptions = .transitionCrossDissolve,
         completion: ((Bool) -> Void)? = nil
     ) {
-        guard self.alpha.isZero
-        else { return }
-        
-        UIView.animate(
-            withDuration: duration,
-            delay: delay,
-            options: options,
-            animations: { self.alpha = .one },
-            completion: completion)
+        guard let containerView = containerView ?? self.superview
+        else {
+            self.isHidden = false
+            print("부모 뷰가 없어 페이드 인 실패")
+            return
+        }
+
+        UIView.transition(
+            with: containerView,
+            duration: duration,
+            // 애니메이션 효과에 필수적이라 인자를 직접 넣는 경우에도 누락 방지를 위해 union 처리
+            options: options.union([.transitionCrossDissolve]),
+            animations: { [weak self] in self?.isHidden = false },
+            completion: completion
+        )
     }
     
-    /// alpha 를 사용한 페이드 아웃 애니메이션: 디폴트 지속 시간 0.2, 딜레이 0, curveEaseInOut, completion nil
+    /// 해당 뷰를 애니메이션 효과와 함께 숨김(IsHidden = true)
+    ///
+    /// 부모 뷰가 없는 경우 애니메이션 효과 없이 isHidden = true 로만 변경
+    /// - Parameters:
+    ///   - containerView: 해당 뷰의 부모 뷰, 입력하지 않으면 해당 뷰의 부모 뷰에 self.superview로 접근
+    ///   - duration: 애니메이션 지속 시간, 기본 값: 0.2
+    ///   - delay: 딜레이 시간, 기본 값: 0
+    ///   - options: 애니메이션 옵션, 기본 값: transitionCrossDissolve
+    ///   - completion: 애니메이션 종료 후 할 작업, 기본 값: nil
     func fadeOut(
-        withDuration duration: TimeInterval = animationDuration,
+        containerView: UIView? = nil,
+        duration: TimeInterval = animationDuration,
         delay: TimeInterval = .zero,
-        options: AnimationOptions = AnimationOptions.curveEaseInOut,
+        options: AnimationOptions = .transitionCrossDissolve,
         completion: ((Bool) -> Void)? = nil
     ) {
-        guard self.alpha.isNormal
-        else { return }
-        
-        UIView.animate(
-            withDuration: duration,
-            delay: delay,
-            options: options,
-            animations: { self.alpha = .zero },
+        guard let containerView = containerView ?? self.superview
+        else {
+            self.isHidden = true
+            print("부모 뷰가 없어 페이드 아웃 실패")
+            return
+        }
+
+        UIView.transition(
+            with: containerView,
+            duration: duration,
+            /// 애니메이션 효과에 필수적이라 인자를 직접 넣는 경우에도 누락 방지를 위해 union 처리
+            options: options.union([.transitionCrossDissolve]),
+            animations: { [weak self] in self?.isHidden = true },
             completion: completion
         )
     }
