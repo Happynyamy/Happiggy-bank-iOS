ed6a091679ddba7071edd69fd1efc7d3aa78e5d0
sun <skkimeo@gmail.com>
2022-12-25 23:37:49 +0900

    [refactor] 커스텀 폰트 적용을 위한 베이스 클래스 생성 #263

---
+++ Happiggy-bank/Happiggy-bank/Subview/BaseButton.swift
@@ -0,0 +1,13 @@
+//
+//  BaseButton.swift
+//  Happiggy-bank
+//
+//  Created by sun on 2022/12/25.
+//
+
+import UIKit
+
+/// 커스텀 폰트를 적용하기 위한 UIButton
+final class BaseButton: UIButton {
+
+}
---
+++ Happiggy-bank/Happiggy-bank/Subview/BaseTextView.swift
@@ -0,0 +1,13 @@
+//
+//  BaseTextView.swift
+//  Happiggy-bank
+//
+//  Created by sun on 2022/12/25.
+//
+
+import UIKit
+
+/// 커스텀 폰트를 적용하기 위한 UITextView
+final class BaseTextView: UITextView {
+
+}
