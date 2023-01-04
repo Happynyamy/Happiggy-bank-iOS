dadedd78f60c40f9905eaeae128e5561f773f5de
sun <skkimeo@gmail.com>
2023-01-04 10:46:44 +0900

    [refactor] NetworkError의 errorDescription String으로 변경

--- Happiggy-bank/Happiggy-bank/Utils/Network/NetworkError.swift
+++ Happiggy-bank/Happiggy-bank/Utils/Network/NetworkError.swift
@@ -19,14 +19,14 @@ enum NetworkError: LocalizedError {
     var errorDescription: String? {
         switch self {
         case .invalidRequest:
-            return NSLocalizedString("잘못된 요청입니다.", comment: "invalid request")
+            return "잘못된 요청입니다."
         case .transportError:
-            return NSLocalizedString("통신 에러가 발생했습니다", comment: "transport error")
+            return "통신 에러가 발생했습니다"
         case .serverSideError(let statusCode):
             let statusCodeIfNeeded = statusCode != nil ? " 상태코드: \(statusCode!)" : .empty
-            return NSLocalizedString("서버에 에러가 발생했습니다.\(statusCodeIfNeeded)", comment: "server-side error")
+            return "서버에 에러가 발생했습니다.\(statusCodeIfNeeded)"
         case .decodingFailure:
-            return NSLocalizedString("디코딩에 실패했습니다.", comment: "decoding failure")
+            return "디코딩에 실패했습니다."
         }
     }
 }
