394ed43b42cc28280d955986dca996df80de819c
sun <skkimeo@gmail.com>
2023-01-16 00:43:22 +0900

    [refactor] FontSelectionViewController 리팩토링 #281

---
+++ Happiggy-bank/Happiggy-bank/Utils/Extensions/UIViewController/UIViewController+ShowTabBar.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController+showTabBar.swift
+//  Happiggy-bank
+//
+//  Created by sun on 2023/01/16.
+//
+
+import UIKit
+
+extension UIViewController {
+
+    func showTabBar() {
+        self.tabBarController?.tabBar.isHidden = false
+    }
+}
